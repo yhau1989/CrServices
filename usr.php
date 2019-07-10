62515e5e239b9ff55774423a8a6ab4d1e0341e39
DEPRATI\spilay <samuel.pilay@deprati.com.ec>
2019-07-10 02:34:20 -0500

    Se agrega funcionaliad REST a la seccion de Controller de usuarios
    Se agrega funcionaliad REST a la seccion de Controller de usuarios

--- usr.php
+++ usr.php
@@ -3,6 +3,15 @@
 require_once "src/dataobject/TUsuarios.php";
 
 
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+$decode = json_decode(file_get_contents("php://input"));
+
+
+
 $ft = new TUsuarios();
 
 var_dump($ft->getUsuarios());
