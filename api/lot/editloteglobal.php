1b2af71b98596b37ae86b6014d314165bbf36628
Samuel Pilay Muñoz <yhau1989@gmail.com>
2019-08-05 04:35:03 -0500

    Update editloteglobal.php

--- api/lot/editloteglobal.php
+++ api/lot/editloteglobal.php
@@ -8,8 +8,24 @@ header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-$ft = new TLotes();
-$response = $ft->getLotesByPendingProcesar();
+$decode = json_decode(file_get_contents("php://input"));
+if ($decode) {
+    if (!(isset($decode) || isset($decode->lote) || isset($decode->lote->tipocambio) || isset($decode->lote->id) || isset($decode->lote->usuario)
+        || isset($decode->lote->fini) || isset($decode->lote->ffin))) {
+        $response = array(
+            'error' => 'error',
+            'mensaje' => 'Parametros de entrada incorrectos.'
+        );
+    } else {
+       
+        $response = 'Hola';
+    }
+} else {
+    $response = array(
+        'error' => 'error',
+        'mensaje' => 'No JSON value set'
+    );
+}
 
 
 $encode = json_encode($response);
